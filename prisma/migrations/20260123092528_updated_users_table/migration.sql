d0f7deca650236f3dfe3ca9287ed1dcbe8eee6c0
Tserpilouski <terpilovskikirill@gmail.com>
2026-01-23 23:46:28 +0100

    feat: added new tables to db

---
+++ prisma/migrations/20260123092528_updated_users_table/migration.sql
@@ -0,0 +1,48 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `genres` on the `Movie` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "Movie" DROP COLUMN "genres";
+
+-- AlterTable
+DELETE FROM "User";
+
+ALTER TABLE "User" DROP COLUMN "createAt",
+DROP COLUMN "password",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "passwordHash" TEXT NOT NULL,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
+
+-- CreateTable
+CREATE TABLE "Genre" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MovieGenre" (
+    "movieId" TEXT NOT NULL,
+    "genreId" INTEGER NOT NULL,
+
+    CONSTRAINT "MovieGenre_pkey" PRIMARY KEY ("movieId","genreId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Genre_name_key" ON "Genre"("name");
+
+-- AddForeignKey
+ALTER TABLE "MovieGenre" ADD CONSTRAINT "MovieGenre_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MovieGenre" ADD CONSTRAINT "MovieGenre_genreId_fkey" FOREIGN KEY ("genreId") REFERENCES "Genre"("id") ON DELETE CASCADE ON UPDATE CASCADE;
